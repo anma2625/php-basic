cda29776cee09d1cc860adcba532372448ccafb7
anma2625 <harumun0720@yahoo.co.jp>
2025-06-16 09:35:21 +0900

    Add files via upload

---
+++ foreach.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $user_names = ['侍太郎','侍一郎','侍二郎','侍三郎','侍四郎'];
+
+    //配列$user_namesの要素を１つずつ順番に出力する
+    foreach ($user_names as $user_name){
+      echo $user_name . '<br>';
+    }
+    ?>
+  </p>
+  <p>
+    <?php
+    $personal_data =['name' => '侍太郎','age' => 36,'gender' => '男性']
+  
+    //連想配列$personal_dataのキーと値を１つずつ順番に出力する
+    foreach ($personal_data as $key => $value) {
+      echo "{$key}は｛$value}です。<br>";
+    }
+
+    //連想配列$personal_dataの値を１つずつ順番に出力する
+    foreach ($personal_data as $value) {
+      echo $value . '<br>';
+    }
+    ?>
+  </p>
+</body>
+</html>
